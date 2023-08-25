7303614fa7dbec80ebfcda40a2771e97f054bdc2
miszelo <michal.kawczak@gmail.com>
2023-08-25 19:21:29 +0200

    [refactor] refactor EsaOseSmogDataResponseTranslator

--- src/main/java/com/example/geodata/translators/EsaOseSmogDataResponseTranslator.java
+++ src/main/java/com/example/geodata/translators/EsaOseSmogDataResponseTranslator.java
@@ -1,5 +1,6 @@
 package com.example.geodata.translators;
 
+import com.example.geodata.esaose.model.School;
 import com.example.geodata.esaose.model.SmogData;
 import com.example.geodata.model.Coordinates;
 import com.example.geodata.model.GeoData;
@@ -12,8 +13,9 @@ public class EsaOseSmogDataResponseTranslator implements ResponseTranslator<Smog
     @Override
     public GeoData translate(SmogData smogData) {
         var data = smogData.getData();
+        var schoolData = smogData.getSchool();
         return GeoData.builder()
-                .place(getPlace(smogData))
+                .place(getPlace(schoolData))
                 .humidity(data.getHumidityAverage())
                 .pressure(data.getPressureAverage())
                 .temperature(data.getTemperatureAverage())
@@ -23,16 +25,20 @@ public class EsaOseSmogDataResponseTranslator implements ResponseTranslator<Smog
                 .build();
     }
 
-    private Place getPlace(SmogData smogData) {
+    private Place getPlace(School schoolData) {
         return Place.builder()
-                .name(smogData.getSchool().getName())
-                .city(smogData.getSchool().getCity())
-                .street(smogData.getSchool().getStreet())
-                .postalCode(smogData.getSchool().getPostalCode())
-                .coordinates(Coordinates.builder()
-                        .longitude(smogData.getSchool().getLongitude())
-                        .latitude(smogData.getSchool().getLatitude())
-                        .build())
+                .name(schoolData.getName())
+                .city(schoolData.getCity())
+                .street(schoolData.getStreet())
+                .postalCode(schoolData.getPostalCode())
+                .coordinates(getCoordinates(schoolData))
+                .build();
+    }
+
+    private Coordinates getCoordinates(School schoolData) {
+        return Coordinates.builder()
+                .longitude(schoolData.getLongitude())
+                .latitude(schoolData.getLatitude())
                 .build();
     }
 }
