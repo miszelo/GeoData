22c7bddcafbe260cfe3150120299e20d0cde515d
miszelo <michal.kawczak@gmail.com>
2023-10-17 15:43:56 +0200

    [fix] deleted CoordinatesRepository.java speed up save data

--- src/main/java/com/example/geodata/repository/PlaceRepository.java
+++ src/main/java/com/example/geodata/repository/PlaceRepository.java
@@ -1,11 +1,11 @@
 package com.example.geodata.repository;
 
-import com.example.geodata.model.Coordinates;
 import com.example.geodata.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    Optional<Place> findFirstByCoordinates(Coordinates coordinates);
+
+    Optional<Place> findFirstByNameAndCityAndStreetAndPostalCode(String name, String city, String street, String postalCode);
 }
--- src/main/java/com/example/geodata/services/SmogDataService.java
+++ src/main/java/com/example/geodata/services/SmogDataService.java
@@ -2,7 +2,6 @@ package com.example.geodata.services;
 
 import com.example.geodata.esaose.EsaOseDataRetriever;
 import com.example.geodata.model.GeoData;
-import com.example.geodata.repository.CoordinatesRepository;
 import com.example.geodata.repository.GeoDataRepository;
 import com.example.geodata.repository.PlaceRepository;
 import com.example.geodata.translators.EsaOseSmogDataResponseTranslator;
@@ -20,18 +19,15 @@ public class SmogDataService {
     private final EsaOseDataRetriever esaOseDataRetriever;
     private final GeoDataRepository geoDataRepository;
     private final PlaceRepository placeRepository;
-    private final CoordinatesRepository coordinatesRepository;
 
     public SmogDataService(EsaOseSmogDataResponseTranslator esaOseSmogDataResponseTranslator,
                            EsaOseDataRetriever esaOseDataRetriever,
                            GeoDataRepository geoDataRepository,
-                           PlaceRepository placeRepository,
-                           CoordinatesRepository coordinatesRepository) {
+                           PlaceRepository placeRepository) {
         this.esaOseSmogDataResponseTranslator = esaOseSmogDataResponseTranslator;
         this.esaOseDataRetriever = esaOseDataRetriever;
         this.geoDataRepository = geoDataRepository;
         this.placeRepository = placeRepository;
-        this.coordinatesRepository = coordinatesRepository;
     }
 
     public List<GeoData> saveData() {
@@ -42,17 +38,18 @@ public class SmogDataService {
         if (geoDataRepository.findFirstByTimestamp(geoData.get(0).getTimestamp()).isPresent()) {
             return Collections.emptyList();
         }
-        for (var data : geoData) {
+
+        geoData.forEach(data -> {
             var place = data.getPlace();
-            var coordinates = place.getCoordinates();
-            var existingCoordinates = coordinatesRepository.findFirstByLongitudeAndLatitude(
-                    coordinates.getLongitude(),
-                    coordinates.getLatitude());
-            var existingPlace = placeRepository.findFirstByCoordinates(existingCoordinates.orElse(null));
-            if (existingPlace.isPresent() && existingCoordinates.isPresent()) {
-                data.setPlace(existingPlace.get());
-            }
-        }
+            var existingPlace = placeRepository.findFirstByNameAndCityAndStreetAndPostalCode(
+                    place.getName(),
+                    place.getCity(),
+                    place.getStreet(),
+                    place.getPostalCode()
+            );
+            existingPlace.ifPresent(data::setPlace);
+        });
+
         geoDataRepository.saveAll(geoData);
         return geoData;
     }
