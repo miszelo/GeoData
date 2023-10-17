d0cfbb07c879ec0c320e68bfccf896e40edb8255
miszelo <michal.kawczak@gmail.com>
2023-10-17 14:52:22 +0200

    [feature] add query methods

--- src/main/java/com/example/geodata/repository/GeoDataRepository.java
+++ src/main/java/com/example/geodata/repository/GeoDataRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GeoDataRepository extends JpaRepository<GeoData, Long> {
 
     Optional<GeoData> findFirstByTimestamp(LocalDateTime timestamp);
+
+    Optional<List<GeoData>> findAllByTimestampAfter(LocalDateTime timestamp);
 }
--- src/main/java/com/example/geodata/repository/PlaceRepository.java
+++ src/main/java/com/example/geodata/repository/PlaceRepository.java
@@ -1,10 +1,11 @@
 package com.example.geodata.repository;
 
+import com.example.geodata.model.Coordinates;
 import com.example.geodata.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    Optional<Place> findFirstByNameAndCityAndStreetAndPostalCode(String placeName, String city, String street, String postalCode);
+    Optional<Place> findFirstByCoordinates(Coordinates coordinates);
 }
