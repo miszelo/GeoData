75d2f78fa26229134e03147ad4d304ec2048d970
miszelo <michal.kawczak@gmail.com>
2023-10-17 21:29:42 +0200

    [feature] implement faster retrieve data from db

--- src/main/java/com/example/geodata/repository/GeoDataRepository.java
+++ src/main/java/com/example/geodata/repository/GeoDataRepository.java
@@ -2,6 +2,7 @@ package com.example.geodata.repository;
 
 import com.example.geodata.model.GeoData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,5 +14,10 @@ public interface GeoDataRepository extends JpaRepository<GeoData, Long> {
 
     Optional<GeoData> findFirstByTimestamp(LocalDateTime timestamp);
 
-    Optional<List<GeoData>> findAllByTimestampAfter(LocalDateTime timestamp);
+    @Query("SELECT g FROM GeoData g" +
+            " JOIN FETCH g.place" +
+            " JOIN FETCH g.place.city" +
+            " JOIN FETCH g.place.coordinates" +
+            " WHERE g.timestamp > ?1")
+    List<GeoData> findAllByTimestampAfter(LocalDateTime timestamp);
 }
