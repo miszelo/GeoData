31476581c523086f572b74cd9601de82842cb20e
miszelo <michal.kawczak@gmail.com>
2023-10-31 16:24:11 +0100

    [feature] add endpoints

--- src/main/java/com/example/geodata/restapi/controllers/SmogDataController.java
+++ src/main/java/com/example/geodata/restapi/controllers/SmogDataController.java
@@ -1,12 +1,15 @@
 package com.example.geodata.restapi.controllers;
 
+import com.example.geodata.restapi.dto.GeoDataDTO;
+import com.example.geodata.restapi.dto.RequestRetrieveDataByLocalDateDto;
 import com.example.geodata.services.SmogDataService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.example.geodata.restapi.ApiConst.API;
 import static com.example.geodata.restapi.ApiConst.API_VERSION;
@@ -21,14 +24,55 @@ public class SmogDataController {
         this.smogDataService = smogDataService;
     }
 
-    @PostMapping(value = "/smog-data")
-    public ResponseEntity<?> saveDataToDatabase() {
+    @PostMapping(value = "/smog-data",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<GeoDataDTO>> saveGeoData() {
         var geoData = smogDataService.saveData();
         return new ResponseEntity<>(geoData, HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/smog-data")
-    public ResponseEntity<?> getCurrentDataFromDatabase() {
+    @DeleteMapping(value = "/smog-data",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deleteGeoData(@RequestParam LocalDateTime localDateTime) {
+        smogDataService.deleteData(localDateTime);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/smog-data/{city}",
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<List<GeoDataDTO>> getGeoDataByCity(@PathVariable String city) {
+//        var geoData = smogDataService.getGeoDataByCity(city);
+//        return new ResponseEntity<>(geoData, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/smog-data/{schoolName}",
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<List<GeoDataDTO>> getGeoDataBySchoolName(@PathVariable String schoolName) {
+//        var geoData = smogDataService.getGeoDataBySchoolName(schoolName);
+//        return new ResponseEntity<>(geoData, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/smog-data/{schoolName}/{timestamp}",
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<List<GeoDataDTO>> getGeoDataBySchoolNameAndTimestamp(
+//            @PathVariable String schoolName,
+//            @PathVariable LocalDateTime timestamp) {
+//        var geoData = smogDataService.getGeoDataBySchoolNameAndTimestamp(schoolName, timestamp);
+//        return new ResponseEntity<>(geoData, HttpStatus.OK);
+//}
+
+    @GetMapping(value = "/smog-data",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getGeoDataByDay(@RequestBody RequestRetrieveDataByLocalDateDto timestamp) {
+        var geoData = smogDataService.getGeoDataByTimestamp(timestamp);
+        return new ResponseEntity<>(geoData, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/smog-data",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<GeoDataDTO>> getGeoData() {
         var geoData = smogDataService.getCurrentGeoData();
         return new ResponseEntity<>(geoData, HttpStatus.OK);
     }
