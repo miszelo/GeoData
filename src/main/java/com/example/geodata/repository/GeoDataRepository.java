5166822b1a36b780730c5a2d0c1237bb66762cff
miszelo <michal.kawczak@gmail.com>
2023-10-31 16:25:15 +0100

    [feature] modify query methods in GeoDataRepository

--- src/main/java/com/example/geodata/repository/GeoDataRepository.java
+++ src/main/java/com/example/geodata/repository/GeoDataRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -14,10 +15,29 @@ public interface GeoDataRepository extends JpaRepository<GeoData, Long> {
 
     Optional<GeoData> findFirstByTimestamp(LocalDateTime timestamp);
 
+    void deleteAllByTimestamp(LocalDateTime timestamp);
+
     @Query("SELECT g FROM GeoData g" +
             " JOIN FETCH g.place" +
             " JOIN FETCH g.place.city" +
             " JOIN FETCH g.place.coordinates" +
             " WHERE g.timestamp > ?1")
-    List<GeoData> findAllByTimestampAfter(LocalDateTime timestamp);
+    Optional<List<GeoData>> findAllByTimestampAfter(LocalDateTime timestamp);
+
+
+    @Query("SELECT g FROM GeoData g" +
+            " JOIN FETCH g.place" +
+            " JOIN FETCH g.place.city" +
+            " JOIN FETCH g.place.coordinates" +
+            " WHERE DATE(g.timestamp) = ?1")
+    List<GeoData> findAllByTimestamp(Timestamp timestamp);
+
+
+    @Query("SELECT g FROM GeoData g" +
+            " JOIN FETCH g.place" +
+            " JOIN FETCH g.place.city" +
+            " JOIN FETCH g.place.coordinates" +
+            " WHERE g.place.city.name = ?1" +
+            " AND g.timestamp > ?2")
+    Optional<List<GeoData>> findAllBySchoolNameAndTimestamp(String schoolName, LocalDateTime timestamp);
 }
